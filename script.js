a02655424b39e057fae04fcb8cd84f28483c7bd0
Deepak negi <dnegi5817@gamil.com>
2026-06-23 02:45:26 +0530

    update the formate and make code more clean

--- script.js
+++ script.js
@@ -6,49 +6,64 @@ let box = document.querySelectorAll(".box");
 //**************select all new tab in the form of node list************************
 let newtab = document.querySelectorAll(".newTab");
 // ****************for back button logic******************
-let back = document.querySelector(".backbutton");
-
-//**********************select single cards**************8 */
-box.forEach(function (elem) {
-	//*****************edd event listener on all single cards********************* */
-	elem.addEventListener("click", function () {
-		//*****************open new tab with the use of index of node list of new tab and the index is same corresponding to id of the corresponding cards*****************
-		newtab[elem.id].style.display = "block";
-		header.style.display = "none";
-		mainContainer.style.display = "none";
-		back.style.display = "block";
-
+function pageOpening() {
+	//**********************select single cards**************8 */
+	box.forEach(function (elem) {
+		//*****************edd event listener on all single cards********************* */
+		elem.addEventListener("click", function () {
+			//*****************open new tab with the use of index of node list of new tab and the index is same corresponding to id of the corresponding cards*****************
+			newtab[elem.id].style.display = "block";
+			header.style.display = "none";
+			mainContainer.style.display = "none";
+			back.style.display = "block";
+		});
 	});
-});
+}
+pageOpening();
 
 //*****************back button logic***************************
-back.addEventListener("click", function () {
-	mainContainer.style.display = "block";
-	header.style.display = "flex";
-	newtab.forEach(function (elem) {
-		elem.style.display = "none";
+function backButton() {
+	let back = document.querySelector(".backbutton");
+	back.addEventListener("click", function () {
+		mainContainer.style.display = "block";
+		header.style.display = "flex";
+		newtab.forEach(function (elem) {
+			elem.style.display = "none";
+		});
+		back.style.display = "none";
 	});
-	back.style.display = "none";
-});
+}
+backButton();
+
 
 //****************theme change code*****************
 let html = document.querySelector("html");
-let themeButton = document.querySelector("#theme-button");
-let currenttheme = "gray";
-themeButton.addEventListener("click", function () {
-	if (currenttheme === "gray") {
-		html.style.setProperty("--bgcurrentcolor", "var(--bglight)");
-		html.style.setProperty("--navcurrentcolor", "var(--navlight)");
-		html.style.setProperty("--textcurrentcolor", "var(--textdark)");
-		currenttheme = "light";
-	} else if (currenttheme == "light") {
-		html.style.setProperty("--bgcurrentcolor", "var(--bgbrown)");
-		html.style.setProperty("--navcurrentcolor", "var(--navbrown)");
-		html.style.setProperty("--textcurrentcolor", "var(--textlight)");
-		currenttheme = "brown";
-	} else if (currenttheme == "brown") {
-		html.style.setProperty("--bgcurrentcolor", "var(--bggray)");
-		html.style.setProperty("--navcurrentcolor", "var(--navgray)");
-		currenttheme = "gray";
-	}
-});
+function changeTheme() {
+	let themeButton = document.querySelector("#theme-button");
+	let currenttheme = "gray";
+	themeButton.addEventListener("click", function () {
+		if (currenttheme === "gray") {
+			html.style.setProperty("--bgcurrentcolor", "var(--bglight)");
+			html.style.setProperty("--navcurrentcolor", "var(--navlight)");
+			html.style.setProperty("--textcurrentcolor", "var(--textdark)");
+			currenttheme = "light";
+		} else if (currenttheme == "light") {
+			html.style.setProperty("--bgcurrentcolor", "var(--bgbrown)");
+			html.style.setProperty("--navcurrentcolor", "var(--navbrown)");
+			html.style.setProperty("--textcurrentcolor", "var(--textlight)");
+			currenttheme = "brown";
+		} else if (currenttheme == "brown") {
+			html.style.setProperty("--bgcurrentcolor", "var(--bggray)");
+			html.style.setProperty("--navcurrentcolor", "var(--navgray)");
+			currenttheme = "gray";
+		}
+	});
+}
+changeTheme();
+
+
+// *************************to do list form fill****************
+// let enterButton = document.querySelector('.enterButton');
+// let storage = [
+// 	{
+// ]
